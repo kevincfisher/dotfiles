6e851c319090ff0d5c74330f93bdcda1f202bea2
Kevin Fisher <kevincliffordfisher@gmail.com>
2025-11-19 23:55:17 -0500

    add README

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
+-- TODO: This file is kind of pointless. Either move keymaps here or remove this file.
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>o", "<Cmd>Oil</Cmd>", { desc = "Open Oil File Tree" })
