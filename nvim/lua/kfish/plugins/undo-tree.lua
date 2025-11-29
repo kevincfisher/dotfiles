83e6bc4290f515a70c34eba6a6ab7dca3bae90d2
Kevin Fisher <kevincliffordfisher@gmail.com>
2025-11-28 22:42:09 -0500

    focus undotree on toggle

--- nvim/lua/kfish/plugins/undo-tree.lua
+++ nvim/lua/kfish/plugins/undo-tree.lua
@@ -1,6 +1,7 @@
 return {
 	"mbbill/undotree",
 	config = function()
-		vim.keymap.set("n", "<leader>u", vim.cmd.UndoTreeToggle, { desc = "Toggle UndoTree" })
+		vim.g.undotree_SetFocusWhenToggle = true
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
 	end,
 }
