068cc986ed6e34336dd020bb6c374dfabb9be804
Kevin Fisher <kevincliffordfisher@gmail.com>
2026-09-15 20:09:08 -0400

    fix: switch to copilot.lua (#13)

--- home/.config/nvim/lua/plugins/copilot.lua
+++ home/.config/nvim/lua/plugins/copilot.lua
@@ -1,9 +1,41 @@
 vim.pack.add({
-  "https://github.com/github/copilot.vim"
+	"https://github.com/copilotlsp-nvim/copilot-lsp",
+	"https://github.com/zbirenbaum/copilot.lua",
 })
 
-vim.keymap.set("n", "<leader>ai", "<cmd>Copilot panel<CR>", { desc = "Open Copliot Panel"})
-vim.keymap.set("n", "<leader>ae", "<cmd>Copilot enable<CR>", { desc = "Enable Copliot"})
-vim.keymap.set("n", "<leader>ad", "<cmd>Copilot disable<CR>", { desc = "Disable Copliot"})
+local copilot = require("copilot")
 
+local grp = vim.api.nvim_create_augroup("Copilot", { clear = true })
+vim.api.nvim_create_autocmd("InsertEnter", {
+	pattern = "*",
+	group = grp,
+  once = true,
+	callback = function()
+		copilot.setup({
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				keymap = {
+					accept = "<Tab>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+		})
+	end,
+})
 
+-- toggle copilot suggestions when completion menu is visible
+vim.api.nvim_create_autocmd("User", {
+  pattern="BlinkCmpMenuOpen",
+  callback=function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuClose",
+  callback = function ()
+    vim.b.copilot_suggestion_hidden = false
+  end
+})
